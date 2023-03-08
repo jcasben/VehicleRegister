05af5b8c21ac4058c437ff334dacb0e456986dcb
Jesus Castillo <jcasben02@gmail.com>
2023-03-08 16:25:02 +0100

    [UPDATE] Development of some methods

--- src/main/utils/Register.java
+++ src/main/utils/Register.java
@@ -1,4 +1,29 @@
 package main.utils;
 
+import main.vehicles.*;
+import main.io.ObjectWriter;
+
 public class Register {
+
+    private final Vehicle [] REGISTERED = new Vehicle[10];
+    private int inRegister = 0;
+    private ObjectWriter writer = new ObjectWriter();
+
+    public void addCar(String brand, String colour, String carLicenseNumber) {
+        REGISTERED[inRegister] = new Car(brand, colour, carLicenseNumber);
+        writer.wrtObj(REGISTERED[inRegister]);
+        inRegister++;
+    }
+
+    public void addMotorbike(String brand, String colour, String carLicenseNumber) {
+        REGISTERED[inRegister] = new Motorbike(brand, colour, carLicenseNumber);
+        writer.wrtObj(REGISTERED[inRegister]);
+        inRegister++;
+    }
+
+    public void addVan(String brand, String colour, String carLicenseNumber, int passengers) {
+        REGISTERED[inRegister] = new Van(brand, colour, carLicenseNumber, passengers);
+        writer.wrtObj(REGISTERED[inRegister]);
+        inRegister++;
+    }
 }
