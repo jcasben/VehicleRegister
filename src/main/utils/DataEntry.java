2f06a0694d0e2c72286f37a75a888452230c5983
Jesus Castillo <jcasben02@gmail.com>
2023-03-08 16:25:45 +0100

    [NEW] Added class DataEntry

---
+++ src/main/utils/DataEntry.java
@@ -0,0 +1,13 @@
+package main.utils;
+
+import java.util.Scanner;
+
+public class DataEntry {
+
+    private final Scanner IN = new Scanner(System.in);
+
+    public String askBrand() {
+        System.out.print("Introduce the brand of the vehicle: ");
+        return "";
+    }
+}
